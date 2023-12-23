9c3607d3b2102b285a3542525dd1464cb52deded
t0k4r <mi03to08@gmail.com.com>
2023-12-23 23:25:01 +0100

    rewrite

--- qb.go
+++ qb.go
@@ -4,231 +4,37 @@ import (
 	"database/sql"
 	"fmt"
 	"strings"
+	"time"
 )
-
-type Arg string
-
-type Conflict string
-
-const (
-	Ignore  Conflict = "ignore"
-	Replace Conflict = "replace"
-	none    Conflict = "none"
-)
-
-type QInsert struct {
-	table string
-	cols  strings.Builder
-	vals  strings.Builder
-}
-
-func Insert(into string) *QInsert {
-	return &QInsert{
-		table: into,
-		cols:  strings.Builder{},
-		vals:  strings.Builder{},
-	}
-}
-
-func (i *QInsert) insComma() {
-	if i.cols.Len() != 0 {
-		i.cols.WriteString(", ")
-		i.vals.WriteString(", ")
-	}
-}
-
-func (i *QInsert) Col(col string, value any) *QInsert {
-	switch value := value.(type) {
-	case *int, *uint, *float32, *float64:
-		if value != nil {
-			i.insComma()
-			i.cols.WriteString(col)
-			i.vals.WriteString(fmt.Sprint(value))
-		}
-	case int, uint, float32, float64:
-		i.insComma()
-		i.cols.WriteString(col)
-		i.vals.WriteString(fmt.Sprint(value))
-	case *string:
-		if value != nil && *value != "" {
-			i.insComma()
-			i.cols.WriteString(col)
-			i.vals.WriteString("'")
-			i.vals.WriteString(strings.ReplaceAll(*value, "'", "''"))
-			i.vals.WriteString("'")
-		}
-	case string:
-		if value != "" {
-			i.insComma()
-			i.cols.WriteString(col)
-			i.vals.WriteString("'")
-			i.vals.WriteString(strings.ReplaceAll(value, "'", "''"))
-			i.vals.WriteString("'")
-		}
-	case Arg:
-		i.insComma()
-		i.cols.WriteString(col)
-		i.vals.WriteString(string(value))
-
-	case *QSelect:
-		i.insComma()
-		i.cols.WriteString(col)
-		i.vals.WriteString("(")
-		i.vals.WriteString(value.Sql())
-		i.vals.WriteString(")")
-	default:
-		panic(value)
-	}
-	return i
-}
-
-func (i *QInsert) Sql(mod ...Conflict) string {
-	var query strings.Builder
-	var fmod Conflict
-	if len(mod) != 0 {
-		fmod = mod[0]
-	} else {
-		fmod = none
-	}
-	query.WriteString("insert into ")
-	query.WriteString(i.table)
-	query.WriteString("(")
-	query.WriteString(i.cols.String())
-	query.WriteString(") values ")
-	query.WriteString("(")
-	query.WriteString(i.vals.String())
-	switch fmod {
-	case Ignore:
-		query.WriteString(") on conflict do nothing ")
-	case Replace:
-		query.WriteString(") on conflict do update set ")
-		strip := strings.ReplaceAll(i.cols.String(), " ", "")
-		cols := strings.Split(strip, ",")
-		for i, col := range cols {
-			if i != 0 {
-				query.WriteString(", ")
-			}
-			query.WriteString(fmt.Sprintf("%v=EXCLUDED.%v", col, col))
-			_ = col
-		}
-	case none:
-		query.WriteString(")")
-	}
-	return query.String()
-}
-
-type Insertable interface {
-	Dump() (cols []string, vals []any)
-}
-
-func Exec[T Insertable](item T, into string, db *sql.DB) error {
-	q := Insert(into)
-	cols, vals := item.Dump()
-	for i, col := range cols {
-		val := vals[i]
-		q.Col(col, val)
-	}
-	_, err := db.Exec(q.Sql())
-	return err
-}
-
-type QSelect struct {
-	table   string
-	col     strings.Builder
-	join    strings.Builder
-	where   strings.Builder
-	limit   strings.Builder
-	orderBy strings.Builder
-}
-
-func Select(from string) *QSelect {
-	return &QSelect{table: from}
-}
-
-func (s *QSelect) LJoin(table, on string) *QSelect {
-	s.join.WriteString(" left join ")
-	s.join.WriteString(table)
-	s.join.WriteString(" on ")
-	s.join.WriteString(on)
-	return s
-}
-
-func (s *QSelect) RJoin(table, on string) *QSelect {
-	s.join.WriteString(" right join ")
-	s.join.WriteString(table)
-	s.join.WriteString(" on ")
-	s.join.WriteString(on)
-	return s
-}
-
-func (s *QSelect) Join(table, on string) *QSelect {
-	s.join.WriteString(" join ")
-	s.join.WriteString(table)
-	s.join.WriteString(" on ")
-	s.join.WriteString(on)
-	return s
-}
-func (s *QSelect) Cols(cols ...string) *QSelect {
-	for i, col := range cols {
-		if i != 0 {
-			s.col.WriteString(", ")
-		}
-		s.col.WriteString(col)
-	}
-	return s
-}
-func (s *QSelect) Where(where string) *QSelect {
-	s.where.WriteString(where)
-	return s
-}
-
-func (s *QSelect) Wheref(wherefmt string, args ...any) *QSelect {
-	for i, a := range args {
-		switch a := a.(type) {
-		case string:
-			args[i] = strings.ReplaceAll(a, "'", "''")
-		}
-	}
-	s.where.WriteString(fmt.Sprintf(wherefmt, args...))
-	return s
-}
-func (s *QSelect) OrderBy(orderBy string) *QSelect {
-	s.orderBy.WriteString(orderBy)
-	return s
-}
-func (s *QSelect) Limit(limit string) *QSelect {
-	s.limit.WriteString(limit)
-	return s
-}
-func (s *QSelect) Sql() string {
-	var query strings.Builder
-	query.WriteString("select ")
-	query.WriteString(s.col.String())
-	query.WriteString(" from ")
-	query.WriteString(s.table)
-	query.WriteString(s.join.String())
-	if s.where.Len() != 0 {
-		query.WriteString(" where ")
-		query.WriteString(s.where.String())
-	}
-	if s.orderBy.Len() != 0 {
-		query.WriteString(" order by ")
-		query.WriteString(s.orderBy.String())
-	}
-	if s.limit.Len() != 0 {
-		query.WriteString(" limit ")
-		query.WriteString(s.limit.String())
-	}
-	return query.String()
-}
 
 type Selectable interface {
 	Scan(*sql.Rows) (Selectable, error)
 }
 
-func Query[T Selectable](query *QSelect, db *sql.DB, args ...any) ([]T, error) {
+type QSelect[T Selectable] struct {
+	sql strings.Builder
+}
+
+func Select[T Selectable]() *QSelect[T] {
+	return new(QSelect[T])
+}
+
+func (q *QSelect[T]) Add(str string) *QSelect[T] {
+	q.sql.WriteString(str)
+	return q
+}
+
+func (q *QSelect[T]) Addf(format string, a ...any) *QSelect[T] {
+	q.sql.WriteString(fmt.Sprintf(format, a...))
+	return q
+}
+func (q *QSelect[T]) Sql() string {
+	return q.sql.String()
+}
+
+func (q *QSelect[T]) Query(db *sql.DB, args ...any) ([]T, error) {
 	var items []T
-	rows, err := db.Query(query.Sql(), args...)
+	rows, err := db.Query(q.Sql(), args...)
 	if err != nil {
 		return items, err
 	}
@@ -241,4 +47,130 @@ func Query[T Selectable](query *QSelect, db *sql.DB, args ...any) ([]T, error) {
 		items = append(items, item.(T))
 	}
 	return items, nil
+}
+func (q *QSelect[T]) QueryFirst(db *sql.DB, args ...any) (*T, error) {
+	rows, err := db.Query(q.Sql(), args...)
+	if err != nil {
+		return nil, err
+	}
+	var item T
+	if rows.Next() {
+		s, err := item.Scan(rows)
+		if err != nil {
+			return nil, nil
+		}
+		item = s.(T)
+		return &item, nil
+	}
+	return nil, nil
+}
+
+type OnConflict string
+
+const (
+	DoNothing OnConflict = "nothing"
+	DoUpdate  OnConflict = "doUpdate"
+	None      OnConflict = "none"
+)
+
+type QInsert struct {
+	into       string
+	onConflict OnConflict
+	cols       strings.Builder
+	vals       strings.Builder
+}
+
+func Insert(into string) *QInsert {
+	return &QInsert{
+		into:       into,
+		onConflict: None,
+		cols:       strings.Builder{},
+		vals:       strings.Builder{},
+	}
+}
+func (q *QInsert) OnConflict(do OnConflict) *QInsert {
+	q.onConflict = do
+	return q
+}
+func (q *QInsert) write(col string, val string) {
+	if q.cols.Len() == 0 {
+		q.cols.WriteString(col)
+		q.vals.WriteString(val)
+	} else {
+		q.cols.WriteString(", " + col)
+		q.vals.WriteString(", " + val)
+	}
+}
+
+func (q *QInsert) Add(col string, val any) *QInsert {
+	switch val := val.(type) {
+	case nil:
+		q.write(col, "NULL")
+	case []byte:
+		q.write(col, fmt.Sprintf("'%s'", val))
+	case int, int8, int16, int32, int64,
+		uint, uint8, uint16, uint32, uint64:
+		q.write(col, fmt.Sprint(val))
+	case string:
+		if val != "" {
+			q.write(col, strings.ReplaceAll(val, "'", "''"))
+		}
+	case time.Time:
+		q.write(col, fmt.Sprintf("'%v'", val.Format("006-01-0 15:04:05")))
+	case *int, *int8, *int16, *int32, *int64,
+		*uint, *uint8, *uint16, *uint32, *uint64:
+		if val != nil {
+			q.write(col, fmt.Sprint(val))
+		}
+	case *string:
+		if val != nil && *val != "" {
+			q.write(col, strings.ReplaceAll(*val, "'", "''"))
+		}
+	case *time.Time:
+		if val != nil {
+			q.write(col, fmt.Sprintf("'%v'", val.Format("006-01-0 15:04:05")))
+		}
+	default:
+		panic("unknown val type")
+	}
+	return q
+}
+func (q *QInsert) Addf(col string, format string, a ...any) *QInsert {
+	q.write(col, fmt.Sprintf(format, a...))
+	return q
+}
+
+func (q *QInsert) Sql() string {
+	query := strings.Builder{}
+	query.WriteString("insert into ")
+	query.WriteString(q.into)
+	query.WriteString("(")
+	query.WriteString(q.cols.String())
+	query.WriteString(") values ")
+	query.WriteString("(")
+	query.WriteString(q.vals.String())
+	switch q.onConflict {
+	case DoNothing:
+		query.WriteString(") on conflict do nothing ")
+	case DoUpdate:
+		query.WriteString(") on conflict do update set ")
+		strip := strings.ReplaceAll(q.cols.String(), " ", "")
+		cols := strings.Split(strip, ",")
+		for i, col := range cols {
+			if i != 0 {
+				query.WriteString(", ")
+			}
+			query.WriteString(fmt.Sprintf("%v=EXCLUDED.%v", col, col))
+		}
+	case None:
+		query.WriteString(")")
+	default:
+		panic("unknown on conflict")
+	}
+	return query.String()
+}
+
+func (q *QInsert) Exec(db *sql.DB, args ...any) error {
+	_, err := db.Exec(q.Sql(), args...)
+	return err
 }
--- qb_test.go
+++ qb_test.go
@@ -1,32 +1,51 @@
 package qb_test
 
 import (
+	"database/sql"
 	"testing"
 
 	"github.com/t0k4r/qb"
 )
 
+type ds struct {
+}
+
+func (ds) Scan(*sql.Rows) (qb.Selectable, error) {
+	return nil, nil
+}
+
 func TestSelect(t *testing.T) {
-	q := qb.Select("animes a").
-		Cols("a.id", "a.title", "ai.aired").
-		LJoin("anime_infos ai", "ai.anime_id=a.id").
-		Where("a.title like ? and ai.aired > ?").
-		OrderBy("ai.aired desc").
-		Limit("10").
+	q := qb.Select[ds]().
+		Add(" select a.title from animes a ").
+		Addf(" where a.title = '%v' ", "ok").
 		Sql()
 	t.Log(q)
+	// q := qb.Select("animes a").
+	// 	Cols("a.id", "a.title", "ai.aired").
+	// 	LJoin("anime_infos ai", "ai.anime_id=a.id").
+	// 	Where("a.title like ? and ai.aired > ?").
+	// 	OrderBy("ai.aired desc").
+	// 	Limit("10").
+	// 	Sql()
+	// t.Log(q)
 }
 
 func TestInsert(t *testing.T) {
 	q := qb.Insert("animes").
-		Col("title", "cowboy").
-		Col("aired", qb.Arg("$1")).
-		Col("aired_id",
-			qb.Select("anime_infos ai").
-				Cols("ai.aired_id").
-				Where("ai.genre_id = $2")).
-		Col("xd", "").
-		Sql(qb.Replace)
+		OnConflict(qb.DoUpdate).
+		Add("title", "cowboy").
+		Addf("aired", "$1").
+		Sql()
 	t.Log(q)
+	// q := qb.Insert("animes").
+	// 	Col("title", "cowboy").
+	// 	Col("aired", qb.Arg("$1")).
+	// 	Col("aired_id",
+	// 		qb.Select("anime_infos ai").
+	// 			Cols("ai.aired_id").
+	// 			Where("ai.genre_id = $2")).
+	// 	Col("xd", "").
+	// 	Sql(qb.Replace)
+	// t.Log(q)
 
 }
